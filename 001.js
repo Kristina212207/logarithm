6d301676b691b5f51812c0ea989a857e4c0ae264
Kristina212207 <128428842+Kristina212207@users.noreply.github.com>
2023-08-21 17:18:25 +0300

    Update 001.js

--- 001.js
+++ 001.js
@@ -1,16 +1,17 @@
 let start = Date.now();
 let difference = 0;
 let diff2 = 0;
-for (var i = 0; i < 10000; i++) {
+let count = 0;
+for (var i = 0; i < 100000000; i++) {
   var log = Math.log(i);
-  console.log(log);
+  count++;
   var finish = Date.now();
   diff2 = difference;
   difference = Math.floor((finish - start) / 1000);
   if (difference != diff2) {
-    console.log(`Прошло ${difference} сек`);
+    console.log(`Просчитано ${count} чисел. Прошло ${difference} сек.`);
     console.log("=======================================================");
   }
 }
 difference = (finish - start) / 1000;
-console.log(`В итоге рассчитано ${i} чисел, а прошло ${difference} сек`);
+console.log(`В итоге рассчитано ${i} чисел, а прошло ${difference} сек.`);
